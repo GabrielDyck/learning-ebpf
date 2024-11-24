f6ab3be0bb10e6c0b0bb1ba5a67f82726204ec8e
GabrielFernandoDyck <gabrielfernandodyck@gmail.com>
2024-11-24 18:44:57 -0300

    working ok

--- chapter2/hello.py
+++ chapter2/hello.py
@@ -1,6 +1,7 @@
-#!/usr/bin/python3  
+#!/usr/bin/python3
 from bcc import BPF
 
+# Runs in the kernel. Written in C code
 program = r"""
 int hello(void *ctx) {
     bpf_trace_printk("Hello World!");
@@ -8,8 +9,9 @@ int hello(void *ctx) {
 }
 """
 
-b = BPF(text=program)
-syscall = b.get_syscall_fnname("execve")
-b.attach_kprobe(event=syscall, fn_name="hello")
+#Runs in the user namespace
+b = BPF(text=program) # This compile the C code before can be executed.
+syscall = b.get_syscall_fnname("execve")  # We attach the eBPF program to this event.
+b.attach_kprobe(event=syscall, fn_name="hello") # We attach the hello function to the syscall
 
 b.trace_print()
