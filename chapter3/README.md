7aad08c766c369c6cb870a9d285e60c5d00adee7
GabrielDyck <gabrielfernandodyck@gmail.com>
2024-12-01 09:26:47 -0300

    WIP Chapter 3

--- chapter3/README.md
+++ chapter3/README.md
@@ -6,3 +6,18 @@ main [README file](../README.md).
 You should then be able to build the example code as an object file by running
 `make` in this directory. See Chapter 3 of the book for instructions on what to
 do with this object file.
+
+
+
+## BPF Architecture Details:
+
+* https://origin.kernel.org/doc/html/latest/bpf/
+* https://docs.cilium.io/en/stable/reference-guides/bpf/index.html
+
+
+## XDP
+* https://www.datadoghq.com/blog/xdp-intro/
+
+
+## eBPF OPCodes
+* https://oreil.ly/nLbLp
--- chapter3/hello.bpf.c
+++ chapter3/hello.bpf.c
@@ -3,11 +3,13 @@
 
 int counter = 0;
 
-SEC("xdp")
+SEC("xdp")  // The macro SEC() defines a section called xdp that you'll be able to see in the compiled object file.
+            // But for now you can simply think of it as defining that it's express data path(XDP) type of ebpf program.
 int hello(struct xdp_md *ctx) {
-    bpf_printk("Hello World %d", counter);
+    bpf_printk("Hello World %d", counter); // bpf_trace_printk() is the BCC version of this function. In summary, it's the same func.
     counter++; 
     return XDP_PASS;
 }
 
+// We net to define the license string, this is a crucial requrement foir eBPF programs.
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
